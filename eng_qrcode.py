0735daabe30000dfc182bcfa9516c31afc6e14d2
BurakHINGE <burakmentese16@gmail.com>
2025-12-16 14:53:22 +0300

    Update eng_qrcode.py
    File extension problem has been fixed.

--- eng_qrcode.py
+++ eng_qrcode.py
@@ -1,5 +1,13 @@
 import qrcode
 
-img = qrcode.make("Your link")
-img.save("QRCode.png")
-print("Saved as QRCode.png")
+link = input("Enter the link to be converted into a QR code: ")
+
+filename = input("Enter the filename to save the QR code: ")
+
+if ".png" not in filename:
+    filename += ".png"
+
+img = qrcode.make(link)
+img.save(filename)
+
+print(f"Saved as {filename}.")
